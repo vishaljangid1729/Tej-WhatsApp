2e091304507dc7f3fcdb29f272f00072fe5df47d
Vishal Jangid <jangidvishal1999@gmail.com>
2022-03-27 12:12:05 +0530

    removed unwanted import

--- gui/main.py
+++ gui/main.py
@@ -5,7 +5,6 @@ import os
 import sys
 import time
 from tkinter import messagebox
-from unittest import expectedFailure
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
